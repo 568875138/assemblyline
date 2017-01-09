6fea901513d1574b2b68e83fb1a4eaf37025d578
sgaron-cse <sgaron.cse@gmail.com>
2017-01-09 08:18:33 -0500

    Make git-pull.sh script riak independant.

--- al/run/git-pull.sh
+++ al/run/git-pull.sh
@@ -2,27 +2,13 @@
 
 . /etc/default/al
 
-branch() {
-    {
-        python - $@ 2>&1 | grep '^BRANCH:' | sed -e 's/BRANCH: //g' | sort
-    } <<EOF
-import os, sys
-from assemblyline.al.common import forge
-datastore = forge.get_datastore()
-bootstrap = datastore.get_blob('seed')
-repo = bootstrap.get('system', {}).get('internal_repository', {})
-print "BRANCH: " + os.environ.get('AL_BRANCH', repo.get('branch', 'master'))
-EOF
-}
-
 git_update() {
     repo_path=$1
     repo=$2
-    branch=$3
+    branch=`cd ${repo_path} && git branch | grep "*" | tr -d "* "`
     if [ -d ${repo_path} ]; then
-        echo "Updating ${repo} (${branch}):"
+        echo "Updating ${repo} ($branch):"
         (cd ${repo_path} &&
-         git checkout --force ${branch} &&
          git fetch --all &&
          git reset --hard origin/${branch}) 2>&1 |
         grep -Ev '^(Already on|Your branch is up-to-date with) ' |
@@ -32,15 +18,16 @@ git_update() {
     fi
 }
 
-branch=`branch`
-for repo in 'assemblyline' 'al_ui' 'al_private';
+for repo_path in ${PYTHONPATH}/*;
 do
-    repo_path=${PYTHONPATH}/${repo}
-    git_update ${repo_path} ${repo} $branch
+    repo=`echo ${repo_path} | sed -e "s|${PYTHONPATH}/||g"`
+    if [ $repo != "al_services" ]; then
+        git_update ${repo_path} ${repo}
+    fi
 done
 
-for svc in ${PYTHONPATH}/al_services/*;
+for repo_path in ${PYTHONPATH}/al_services/*;
 do
-    repo=`echo $svc | sed -e "s|${PYTHONPATH}/al_services/||g"`
-    git_update $svc $repo $branch
+    repo=`echo ${repo_path} | sed -e "s|${PYTHONPATH}/al_services/||g"`
+    git_update ${repo_path} $repo
 done
