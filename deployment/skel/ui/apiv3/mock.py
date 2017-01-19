fd3cc5a077451a5812c78efd25fd5cc8c7b6e5b5
sgaron-cse <sgaron.cse@gmail.com>
2017-01-19 15:26:46 -0500

    Finished replacing myapi by mock

--- deployment/skel/ui/apiv3/mock.py
+++ deployment/skel/ui/apiv3/mock.py
@@ -2,15 +2,15 @@ from assemblyline.al.common import forge
 from al_ui.apiv3 import core
 from al_ui.api_base import api_login, make_api_response
 
-SUB_API = 'myapi'
+SUB_API = 'mock'
 
 Classification = forge.get_classification()
 
-myapi_api = core.make_subapi_blueprint(SUB_API)
-myapi_api._doc = "My custom API"
+mock_api = core.make_subapi_blueprint(SUB_API)
+mock_api._doc = "Mock API"
 
 
-@myapi_api.route("/<value>/", methods=["GET"])
+@mock_api.route("/<value>/", methods=["GET"])
 @api_login()
 def replay_value(value, **kwargs):
     """
