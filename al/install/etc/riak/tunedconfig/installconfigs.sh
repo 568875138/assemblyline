5c9877b9b6caf5eae0f0ff0222458be3a16da5f6
sgaron-cse <sgaron.cse@gmail.com>
2017-05-15 08:31:52 -0400

    WARNING: There was a discrepancy between SOLR version in riak and solrconfig schemas. You need to run to following command to fix your current installs on each riak nodes:  (cd /opt/al/pkg/assemblyline/al/install/etc/riak/tunedconfig/ && sudo ./installconfigs.sh && sudo service riak restart)

--- al/install/etc/riak/tunedconfig/installconfigs.sh
+++ al/install/etc/riak/tunedconfig/installconfigs.sh
@@ -6,12 +6,4 @@ do
     cp solrconfig.xml.${bucket} /var/lib/riak/yz/${bucket}/conf/solrconfig.xml
 done
 
-#YOK_DIR=/usr/lib/riak/lib/yokozuna-2.0.0-34-g122659d/priv/solr/etc/
-#cp  ${YOK_DIR}/jetty.xml ${YOK_DIR}/jetty.xml.bak
-#cp ./jetty.xml ${YOK_DIR}/jetty.xml
-
-
-#IBROWSE_DIR=/usr/lib/riak/lib/ibrowse-4.0.1/priv/
-#cp ${IBROWSE_DIR}/ibrowse.conf ${IBROWSE_DIR}/ibrowse.conf.bak
-#cp ./ibrowse.conf ${IBROWSE_DIR}/ibrowse.conf
 
