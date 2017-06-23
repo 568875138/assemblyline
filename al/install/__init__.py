18f6cddb4c68d9964eac4006cad9f35c56c14e6c
ekkerri <ekkerri.cse@gmail.com>
2017-06-23 11:53:25 -0400

    check if yara already installed.

--- al/install/__init__.py
+++ al/install/__init__.py
@@ -570,17 +570,23 @@ class SiteInstaller(object):
         self.runcmd(cmdline)
 
     def install_yara_3(self):
-        wd = os.getcwd()
-        local_yara_support = os.path.join(self.alroot, 'support/yara/')
-        local_yara_python = os.path.join(local_yara_support, 'yara-python-3.6.1.tar.gz')
-        self.fetch_package('yara-python-3.6.1.tar.gz', local_yara_python)
+        # Check if yara-python version already installed
+        try:
+            cmd_output = self.runcmd('pip show yara-python')[1]
+        except:
+            cmd_output = ''
+        if "Version: 3.6.1" not in cmd_output:
+            wd = os.getcwd()
+            local_yara_support = os.path.join(self.alroot, 'support/yara/')
+            local_yara_python = os.path.join(local_yara_support, 'yara-python-3.6.1.tar.gz')
+            self.fetch_package('yara-python-3.6.1.tar.gz', local_yara_python)
 
-        os.chdir(local_yara_support)
-        self.runcmd("tar -zxf yara-python-3.6.1.tar.gz")
-        os.chdir(os.path.join(local_yara_support, "yara-python-3.6.1"))
-        self.runcmd("python setup.py build --enable-dotnet")
-        self.runcmd("sudo python setup.py install")
-        os.chdir(wd)
+            os.chdir(local_yara_support)
+            self.runcmd("tar -zxf yara-python-3.6.1.tar.gz")
+            os.chdir(os.path.join(local_yara_support, "yara-python-3.6.1"))
+            self.runcmd("python setup.py build --enable-dotnet")
+            self.runcmd("sudo python setup.py install")
+            os.chdir(wd)
 
     def install_oracle_java8(self):
         self.milestone("Installing Oracle Java 8...")
